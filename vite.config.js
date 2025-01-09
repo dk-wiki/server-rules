bd91216a26b6234ac76c2b3b7f706ea46b7d9511
dk-wiki <dkgithegreat@gmail.com>
2025-01-09 15:25:38 +1100

    Added react to main

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   base: '/server-rules/', // Use your GitHub repository name here
   build: {
     outDir: 'dist', // Optional: Ensure output goes to the 'dist' folder
   },
-});
+  plugins: [react()],
+})
